5fbb92f25755a62f16b3fd7ac583ff7dcee3783b
Yoshio HANAWA <y@hnw.jp>
2018-02-06 22:48:15 +0900

    .

---
+++ appveyor/build.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set SDK_RUNNER=%PHP_SDK_DIR%\phpsdk-%PHP_BUILD_CRT%-%PLATFORM%.bat
+
+for /f %%F in ("%PHP_SDK_TOOLS_URL%") do set PHP_SDK_TOOLS_FILENAME=%%~nxF
+set PHP_SDK_TOOLS_DIRNAME=php-sdk-binary-tools-%PHP_SDK_TOOLS_FILENAME:.zip=%
+
+if not exist "%PHP_SDK_DIR%" (
+	echo Creating %PHP_SDK_DIR%
+	mkdir "%PHP_SDK_DIR%"
+)
+
+wget %PHP_SDK_TOOLS_URL%
+
+7z x -y %PHP_SDK_TOOLS_FILENAME% -o%PHP_SDK_DIR%
+
+if not exist "%SDK_RUNNER%" (
+	echo "%SDK_RUNNER%" doesn't exist
+	exit /b 3
+)
+
+cmd /c %SDK_RUNNER% -t %APPVEYOR_BUILD_FOLDER%\appveyor\build_task.bat
+if %errorlevel% neq 0 exit /b 3
+
+exit /b 0
