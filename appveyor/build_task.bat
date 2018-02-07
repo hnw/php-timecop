b463d0475166990ee5202ccb458a6dce052b3205
Yoshio HANAWA <y@hnw.jp>
2018-02-07 10:29:32 +0900

    .

--- appveyor/build_task.bat
+++ appveyor/build_task.bat
@@ -19,7 +19,10 @@ set PHP_SRC_DIRNAME=%PHP_SRC_FILENAME:.tar.bz2=%
 set PHP_BUILD_ROOT=%PHP_SDK_DIR%\phpdev\%PHP_BUILD_CRT%\%PLATFORM%\%PHP_SRC_DIRNAME%
 
 cd %PHP_SDK_DIR%
+if %errorlevel% neq 0 exit /b 3
+
 if exist %PHP_SDK_TOOLS_DIRNAME% ( cd %PHP_SDK_TOOLS_DIRNAME% )
+if %errorlevel% neq 0 exit /b 3
 
 cmd /c phpsdk_buildtree.bat phpdev
 if %errorlevel% neq 0 exit /b 3
@@ -28,13 +31,17 @@ wget %PHP_SRC_URL%
 if %errorlevel% neq 0 exit /b 3
 
 mkdir %PHP_BUILD_ROOT%
+if %errorlevel% neq 0 exit /b 3
 
 7z x -so %PHP_SRC_FILENAME% | 7z x -si -ttar -o%PHP_BUILD_ROOT%\..
 if %errorlevel% neq 0 exit /b 3
 
-
 set DEPS_DIR=%PHP_SDK_DIR%\deps-%PHP_SRC_DIRNAME%-%PHP_SDK_VC%-%PHP_SDK_ARCH%
 echo Downloading dependencies in %DEPS_DIR%
+
+cd %PHP_BUILD_ROOT%
+if %errorlevel% neq 0 exit /b 3
+
 cmd /c phpsdk_deps -u --deps %DEPS_DIR%
 if %errorlevel% neq 0 exit /b 3
 
