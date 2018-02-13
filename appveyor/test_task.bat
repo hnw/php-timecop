ddef7319091c596bbebfd49183bb1843200755fb
Yoshio HANAWA <y@hnw.jp>
2018-02-13 21:49:11 +0900

    .

--- appveyor/test_task.bat
+++ appveyor/test_task.bat
@@ -5,7 +5,13 @@ set REPORT_EXIT_STATUS=1
 set SKIP_IO_CAPTURE_TESTS=1
 set TEST_PHP_EXECUTABLE=%APPVEYOR_BUILD_FOLDER%\bin\php.exe
 
+if not exist "%TEST_PHP_EXECUTABLE%" (
+    echo "%TEST_PHP_EXECUTABLE%" doesn't exist
+    exit /b 3
+)
+
 "%TEST_PHP_EXECUTABLE%" %APPVEYOR_BUILD_FOLDER%\run-tests.php --show-diff
+if %errorlevel% neq 0 exit /b 3
 
 exit /b %EXIT_CODE%
 
